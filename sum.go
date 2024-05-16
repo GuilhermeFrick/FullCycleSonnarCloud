0c128b78b9709c5c6febb62e93ff1298c1e51210
GuilhermeFrick <frickoliveira.ee@gmail.com>
2024-05-16 01:58:20 -0300

    feat: Add files

---
+++ sum.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(sum(2, 2))
+}
+
+func sum(a int, b int) int {
+
+	return a + b
+}
+
+func sub(a int, b int) int {
+
+	return a - b
+}
+
+func mult(a int, b int) int {
+
+	return a * b
+}
+
+func sumX(a int, b int) int {
+
+	return a + b
+}
+
+func sumy(a int, b int) int {
+
+	return a + b
+}
